ea140bd42c2d966cbbfdd0471635b8f2dbc510e0
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-09 10:44:08 -0300

    feat: test basico de funcionalidade na service

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+  $movie =  app(\App\Services\TMDB\MovieService::class)->getMovieByName('The Matrix');
+  return response()->json($movie);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
