c6d410e44d96be603ba1b4937d8465f95b4d23ff
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-09 12:09:05 -0300

    feat: criando metodo base para pesquisa de livro

---
+++ backend/app/Http/Controllers/MovieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\MovieSearchRequest;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function search(MovieSearchRequest $request)
+    {
+
+    }
+}
