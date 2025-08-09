bf36a4e31962e8aa32b43d0422f5d299b4a2dc22
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-09 10:58:08 -0300

    feat: criando controller de autenticação de usuario

---
+++ backend/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRegisterUserRequest;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register(AuthRegisterUserRequest $request)
+    {
+
+    }
+}
