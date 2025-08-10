5b76d958d06262c3ead3b7faefc6c5ca2a2731c9
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-10 10:57:39 -0300

    feat: adicionando validação e autorização para criação de filmes favoritos

--- backend/app/Http/Controllers/FavoriteMovieController.php
+++ backend/app/Http/Controllers/FavoriteMovieController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\FavoriteMovieStoreRequest;
 use Illuminate\Http\Request;
 
 class FavoriteMovieController extends Controller
 {
-    public function store()
+    public function store(FavoriteMovieStoreRequest $request)
     {
 
     }
