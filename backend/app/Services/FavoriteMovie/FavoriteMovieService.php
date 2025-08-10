fb7e7e00f5eaf4e44ca1c9b39b3a898d6f6d63a5
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-10 12:07:20 -0300

    feat: adicionar método para obter todos os filmes favoritos com suporte a filtro por gênero

--- backend/app/Services/FavoriteMovie/FavoriteMovieService.php
+++ backend/app/Services/FavoriteMovie/FavoriteMovieService.php
@@ -3,15 +3,18 @@
 namespace App\Services\FavoriteMovie;
 
 use App\Models\UserFavoriteMovie;
+use App\Services\TMDB\MovieService;
 use Illuminate\Database\Eloquent\Model;
 
 class FavoriteMovieService
 {
     protected readonly Model $model;
+    protected readonly MovieService $movieService;
 
-    public function __construct(UserFavoriteMovie $model)
+    public function __construct(UserFavoriteMovie $model, MovieService $movieService)
     {
         $this->model = $model;
+        $this->movieService = $movieService;
     }
 
     public function addToFavorites(int $movieId): void
@@ -51,5 +54,42 @@ class FavoriteMovieService
         }
 
         $favorite->delete();
+    }
+
+    public function getAllFavorites(?string $genre = null): array
+    {
+        $user = auth()->user();
+        if (!$user) {
+            throw new \Exception('Usuário não autenticado.');
+        }
+
+        $favorites = $this->model->where('user_id', $user->id)->get();
+        $moviesData = [];
+
+        foreach ($favorites as $favorite) {
+            $movie = $this->movieService->getMovieById($favorite->movie_id);
+
+            if ($movie) {
+                if ($genre) {
+                    $hasGenre = false;
+                    if (!empty($movie['genres'])) {
+                        foreach ($movie['genres'] as $movieGenre) {
+                            if (stripos($movieGenre, $genre) !== false) {
+                                $hasGenre = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if ($hasGenre) {
+                        $moviesData[] = $movie;
+                    }
+                } else {
+                    $moviesData[] = $movie;
+                }
+            }
+        }
+
+        return $moviesData;
     }
 }
