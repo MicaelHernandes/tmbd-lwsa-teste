c6f2827f0b3028021640c884398152486c21abe9
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-10 10:55:52 -0300

    test: adicionando validações para criação de filmes favoritos

--- backend/tests/Feature/Api/UserFavoriteMovie/CreateTest.php
+++ backend/tests/Feature/Api/UserFavoriteMovie/CreateTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+
 describe('Verificação de validações para realizar operação de criação' , function (){
    it('Usuario não autenticado não pode criar favorito', function () {
        $response = $this->postJson(route('favorite_movies.store'), [
@@ -7,5 +9,13 @@ describe('Verificação de validações para realizar operação de criação' ,
        ]);
 
        $response->assertUnauthorized();
+   });
+
+   it('Requisição deve conter o campo movie_id', function () {
+       $user = User::factory()->create();
+       $response = $this->actingAs($user)->postJson(route('favorite_movies.store'));
+
+       $response->assertUnprocessable()
+                ->assertJsonValidationErrors(['movie_id']);
    });
 });
