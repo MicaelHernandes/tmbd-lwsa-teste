2cc12c37455538449ec48f2728d1dbc11289672c
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-10 10:52:32 -0300

    test: atualizando descrição do teste de validação na criação de filmes favoritos

--- backend/tests/Feature/Api/UserFavoriteMovie/CreateTest.php
+++ backend/tests/Feature/Api/UserFavoriteMovie/CreateTest.php
@@ -1,6 +1,6 @@
 <?php
 
-describe('Verificação de autenticação para realizar operação de criação' , function (){
+describe('Verificação de validações para realizar operação de criação' , function (){
    it('Usuario não autenticado não pode criar favorito', function () {
        $response = $this->postJson(route('favorite_movies.store'), [
            'movie_id' => 1,
