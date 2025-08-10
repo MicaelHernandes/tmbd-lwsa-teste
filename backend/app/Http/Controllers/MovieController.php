43f825e28f91a4a83c1ed15604cfae4e8f6449e9
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-10 09:56:08 -0300

    feat: adicionando comportamento no metodo search e instanciando MovieService

--- backend/app/Http/Controllers/MovieController.php
+++ backend/app/Http/Controllers/MovieController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\MovieSearchRequest;
+use App\Services\TMDB\MovieService;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
+    protected readonly MovieService $movieService;
+
+    public function __construct(MovieService $movieService)
+    {
+        $this->movieService = $movieService;
+    }
+
     public function search(MovieSearchRequest $request)
     {
-
+        try{
+            $response = $this->movieService->getMovieByName($request->query('query'));
+            return response()->json(['data' => $response]);
+        }catch (\Throwable $th){
+            return response()->json(['error' => 'Erro ao buscar filmes: '.$th->getMessage()], 500);
+        }
     }
 }
