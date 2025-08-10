306b1a69654a591e0411969c7bcfb935131772fa
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-10 11:12:09 -0300

    feat: adicionar serviço para gerenciar adição de filmes favoritos

--- backend/app/Http/Controllers/FavoriteMovieController.php
+++ backend/app/Http/Controllers/FavoriteMovieController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FavoriteMovieStoreRequest;
+use App\Services\FavoriteMovie\FavoriteMovieService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class FavoriteMovieController extends Controller
 {
-    public function store(FavoriteMovieStoreRequest $request)
-    {
+    private readonly FavoriteMovieService $favoriteMovieService;
 
+    public function __construct(FavoriteMovieService $favoriteMovieService)
+    {
+        $this->favoriteMovieService = $favoriteMovieService;
+    }
+
+    public function store(FavoriteMovieStoreRequest $request) : JsonResponse
+    {
+        try{
+            $this->favoriteMovieService->addToFavorites($request->input('movie_id'));
+            return response()->json(['message' => 'Filme adicionado aos favoritos com sucesso.'], 201);
+        }catch (\Throwable $th){
+            return response()->json([
+                'message' => 'Erro ao adicionar filme aos favoritos.',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
     }
 }
