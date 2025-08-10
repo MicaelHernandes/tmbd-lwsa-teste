8b17692c08d685435e97910d27f76289e303a81f
MicaelHernandes <micaelmacedo100@outlook.com>
2025-08-10 15:11:32 -0300

    feat: adicionando rotas para login e registro

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MoviesView from '@/views/MoviesView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +15,16 @@ const router = createRouter({
           name: 'films',
           component: MoviesView,
         },
+        {
+          path: '/login',
+          name: 'login',
+          component: LoginView
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: RegisterView
+        }
       ],
     },
   ],
